48417b862c6d249496d0553f3f20aee848370e0e
제일 <joimh7@gmail.com>
2024-03-07 15:51:12 +0900

    기본 테스트 코드 작성 완료

--- cypress/e2e/counter.cy.js
+++ cypress/e2e/counter.cy.js
@@ -34,9 +34,25 @@ describe('example counter app', () => {
     })
 
     it("+ 버튼 클릭시 count가 10이 넘는 경우 더이상 증가 못함 (Max == 10)", () => {
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 13; i++) {
             cy.get(".increase-btn").click();
         }
         cy.get("#value").invoke("text").should("eq", "10");
+    });
+
+    it("- 버튼 클릭시 0 밑으로는 감소 못함 (Min == 0)", () => {
+        cy.get(".increase-btn").click();
+        cy.get(".increase-btn").click();
+        for (let i = 0; i < 5; i++) {
+            cy.get(".decrease-btn").click();
+        }
+        cy.get("#value").invoke("text").should("eq", "0");
+    });
+
+    it("reset 버튼 클릭시 0으로 초기화 될 것", () => {
+        cy.get(".increase-btn").click();
+        cy.get(".increase-btn").click();
+        cy.get(".reset-btn").click();
+        cy.get("#value").invoke("text").should("eq", "0");
     });
 })
